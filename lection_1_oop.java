c3c596ef44b592c7163511d1e312624d6e2bc823
Ivan <vanesiotanaev@mail.ru>
2022-12-28 14:57:46 +0300

    oop hw_1 completed

---
+++ lection_1_oop.java
@@ -0,0 +1,25 @@
+public class lection_1_oop {
+    
+    public class PointMethod {
+        static double distance(int x1, int y1, int x2, int y2) {
+            return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        }
+    
+    public class Point2D  {
+        int x, y;        
+    }
+    
+        public static void main(String[] args) {
+    
+            int ax = 0;
+            int ay = 0;
+            int bx = 0;
+            int by = 1;
+            System.out.println(distance(ax, ay, bx, by));
+            // Создать точку 
+            // Печать точки
+        }
+    
+    }
+    
+}
